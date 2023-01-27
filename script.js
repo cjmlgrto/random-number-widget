4b8ba5cc522f6f2c3c9e4c2aab660bbbbcb6a009
Carlos Melegrito <cj@mlgrto.com>
2023-01-27 15:48:16 +1100

    MVP

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+const generate = () => {
+    const result = document.getElementById('result')
+    result.innerText = generate_random_number(1,16)
+}
+
+const generate_random_number = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
